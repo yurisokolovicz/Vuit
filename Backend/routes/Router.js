0e8e65be8a39edb3e51e0df1ed836e9470c9d3b1
yurisokolovicz <yurisokolovicz@gmail.com>
2023-09-09 14:38:55 -0300

    Tratamento de erros nas rotas com try catch

--- Backend/routes/Router.js
+++ Backend/routes/Router.js
@@ -4,14 +4,19 @@ const router = express();
 const data = require("../data.json");
 
 // testando route
-// Get to http://localhost:5000/api/data
 router.get("/", (req, res) => {
   res.send("API esta funcionando!");
 });
 
-// Get to http://localhost:5000/api/data
 router.get("/api/data", (req, res) => {
-  res.json(data);
+  try {
+    if (!data) {
+      throw new Error("Os dados nao estao disponiveis.");
+    }
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
