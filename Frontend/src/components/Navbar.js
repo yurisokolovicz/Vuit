cbb7d0379f612900e1345309d2b99e4765a37462
yurisokolovicz <yurisokolovicz@gmail.com>
2023-09-12 20:39:30 -0300

    Criando componente Navbar

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -1,9 +1,23 @@
 import React from "react";
 
+import { NavLink, Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaArrowRight } from "react-icons/fa";
+
 import "./Navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav id="nav">
+      <GiHamburgerMenu />
+      <FaArrowRight />
+      <ul id="nav-links">
+        <NavLink to="/">VUIT SEGUROS</NavLink>
+        <NavLink to="/cred">VUIT CRED</NavLink>
+        <NavLink to="/beneficios">VUIT BENEFICIOS</NavLink>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
