ec66f1e5002ca718a18004f56e604c26e1a4e68f
yurisokolovicz <yurisokolovicz@gmail.com>
2023-09-12 20:21:02 -0300

    Criando componente Footer

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,19 +1,23 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Home from "./pages/Home/Home";
 
-import "./App.css";
+import Home from "./pages/Home/Home";
 import Cred from "./pages/Home/Cred/Cred";
 import Beneficios from "./pages/Home/Beneficios/Beneficios";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cred" element={<Cred />} />
           <Route path="/beneficios" element={<Beneficios />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ Frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+      <p>Rua Pasteur, 463 - Água Verde -13 andar</p>
+      <p>Curitiba-PR</p>
+    </footer>
+  );
+};
+
+export default Footer;
