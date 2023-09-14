7fdb960fafaa422e5b1e70462acb8e7ee7614305
yurisokolovicz <yurisokolovicz@gmail.com>
2023-09-14 02:50:52 -0300

    Configurando GiHamburgerMenu e FaArrowRight

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { NavLink, Link } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
@@ -7,11 +7,27 @@ import { FaArrowRight } from "react-icons/fa";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [showHamburgerMenu, setShowHamburgerMenu] = useState(true);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+    setShowHamburgerMenu(false);
+    console.log(menuOpen);
+  };
+
+  const toggleHamburger = () => {
+    setShowHamburgerMenu(true);
+  };
+
   return (
     <nav id="nav">
-      <div id="menu">
-        <GiHamburgerMenu id="burguer" />
-        <FaArrowRight id="arrow" />
+      <div id="menu" className={`menu ${menuOpen ? "expanded" : ""}`}>
+        {showHamburgerMenu ? (
+          <GiHamburgerMenu id="burguer" onClick={toggleMenu} />
+        ) : (
+          <FaArrowRight id="arrow" onClick={toggleHamburger} />
+        )}
       </div>
       <div className="container-menu">
         <ul id="nav-links">
