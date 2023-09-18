2eb23a01cc179ba2769ceb62518773d0e60a4f2a
yurisokolovicz <yurisokolovicz@gmail.com>
2023-09-18 14:04:15 -0300

    Separando componente Instagram

--- Frontend/src/components/Contatos/ContatosBeneficios.js
+++ Frontend/src/components/Contatos/ContatosBeneficios.js
@@ -1,40 +1,15 @@
-import React, { useEffect } from "react";
-import { Helmet } from "react-helmet";
-import "./ContatosHome.css";
+import React from "react";
 
-const ContatosHome = () => {
-  useEffect(() => {
-    if (window.instgrm) {
-      window.instgrm.Embeds.process();
-    }
-  }, []);
+import InstagramVuitBeneficios from "../Instagram/InstagramVuitBeneficios";
+import "./ContatosBeneficios.css";
 
+const ContatosBeneficios = () => {
   return (
     <div className="contato-beneficios-container">
-      <Helmet>
-        <script src="https://www.instagram.com/embed.js" async defer />
-      </Helmet>
       <h3>CONTATO</h3>
-      <blockquote
-        className="instagram-media"
-        data-instgrm-permalink="https://www.instagram.com/vuitbeneficios/"
-        data-instgrm-version="12"
-        style={{
-          background: "#FFF",
-          border: 0,
-          borderRadius: "3px",
-          boxShadow: "0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15)",
-          margin: "1px",
-          maxWidth: "700px",
-          minWidth: "94px",
-          padding: 0,
-          width: "700",
-          height: "394",
-          maxHeight: "100%",
-        }}
-      ></blockquote>
+      <InstagramVuitBeneficios />
     </div>
   );
 };
 
-export default ContatosHome;
+export default ContatosBeneficios;
--- Frontend/src/components/Contatos/ContatosCred.js
+++ Frontend/src/components/Contatos/ContatosCred.js
@@ -1,19 +1,11 @@
-import React, { useEffect } from "react";
-import { Helmet } from "react-helmet";
-import "./ContatosHome.css";
+import React from "react";
 
-const ContatosHome = () => {
-  useEffect(() => {
-    if (window.instgrm) {
-      window.instgrm.Embeds.process();
-    }
-  }, []);
+import InstagramVuitCred from "../Instagram/InstagramVuitCred";
+import "./ContatosCred.css";
 
+const ContatosCred = () => {
   return (
     <div className="contato-cred-container">
-      <Helmet>
-        <script src="https://www.instagram.com/embed.js" async defer />
-      </Helmet>
       <div className="contato-cred-text-container">
         <h3>CONTATOS</h3>
         <h4>VAMOS CONVERSAR?</h4>
@@ -22,26 +14,9 @@ const ContatosHome = () => {
         Venha fazer parte deste ecossistema que tem mudado a vida das pessoas.
         Será um prazer tê-los conosco.
       </p>
-      <blockquote
-        className="instagram-media"
-        data-instgrm-permalink="https://www.instagram.com/vuitcred/"
-        data-instgrm-version="12"
-        style={{
-          background: "#FFF",
-          border: 0,
-          borderRadius: "3px",
-          boxShadow: "0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15)",
-          margin: "1px",
-          maxWidth: "700px",
-          minWidth: "394px",
-          padding: 0,
-          width: "700",
-          height: "394",
-          maxHeight: "100%",
-        }}
-      ></blockquote>
+      <InstagramVuitCred />
     </div>
   );
 };
 
-export default ContatosHome;
+export default ContatosCred;
