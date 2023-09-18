33b6b8f76f21bf06c7271c6b7b3287ebebafa26a
yurisokolovicz <yurisokolovicz@gmail.com>
2023-09-18 14:14:07 -0300

    Unir componente Instagram e passar link como prop

--- Frontend/src/components/Contatos/ContatosHome.js
+++ Frontend/src/components/Contatos/ContatosHome.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-import InstagramVuitSeguros from "../Instagram/InstagramVuitSeguros";
+// import InstagramVuitSeguros from "../Instagram/InstagramVuitSeguros";
 import "./ContatosHome.css";
+import Instagram from "../Instagram/Instagram";
 
 const ContatosHome = () => {
   return (
     <div className="contato-container">
       <h3>CONTATOS</h3>
-      <InstagramVuitSeguros />
+      {/* <InstagramVuitSeguros /> */}
+      <Instagram link="https://www.instagram.com/vuitseguros/" />
     </div>
   );
 };
