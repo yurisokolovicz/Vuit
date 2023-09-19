507ab0a50599ef450c85cc2a04ffc6a6de9dc4bb
yurisokolovicz <yurisokolovicz@gmail.com>
2023-09-19 14:10:52 -0300

    Finalizando BotaoBaixePdf

--- Frontend/src/components/Botoes/BotaoBaixePdf/BotaoBaixePdf.js
+++ Frontend/src/components/Botoes/BotaoBaixePdf/BotaoBaixePdf.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 import PdfSvg from "../../CustomSvg/PdfSvg";
+import imagemPdf from "../../../img/Homepage/botao/pdf.png";
+
 import "./BotaoBaixePdf.css";
 
 function BotaoBaixePdf() {
   return (
     <div className="botao-baixepdf">
       <button>
-        <PdfSvg width="24" height="24" /> Baixe PDF apresentação Tudo na Vuit!
+        <div className="icon-div">
+          <img src={imagemPdf} alt="PDF Icon" className="pdf-icon" />{" "}
+        </div>
+        <div className="text-div">
+          <PdfSvg width="24" height="24" /> Baixe PDF apresentação Tudo na Vuit!
+        </div>
       </button>
     </div>
   );
