d1ef0dd2570d21270cfc6fecadd9113dcbc061e1
yurisokolovicz <yurisokolovicz@gmail.com>
2023-09-20 17:17:05 -0300

    Avancando em Comp RetanguloExpansivo

--- Frontend/src/components/RetanguloExpansivo/RetanguloExpansivo.js
+++ Frontend/src/components/RetanguloExpansivo/RetanguloExpansivo.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 
+import { GrFormAdd } from "react-icons/gr";
+import "./RetanguloExpansivo.css";
+
 function RetanguloExpansivo() {
   const [expanded, setExpanded] = useState(false);
 
@@ -9,10 +12,14 @@ function RetanguloExpansivo() {
 
   return (
     <div
+      className="retangulo-container"
       style={{
+        fontSize: "1rem",
+        fontWeight: "400",
+        lineHeight: "1.5",
+        padding: "5px 10px",
         width: "680px",
         border: "1px solid #ccc",
-        padding: "10px",
         marginBottom: "10px",
         cursor: "pointer",
         backgroundColor: "#fff",
@@ -21,9 +28,12 @@ function RetanguloExpansivo() {
       }}
       onClick={toggleExpansion}
     >
-      <div>Seguro de vida em grupo e acidentes pessoais</div>
+      <div className="not-expanded">
+        <p>Seguro de vida em grupo e acidentes pessoais </p>
+        <GrFormAdd className="icon" />
+      </div>
       {expanded && (
-        <div>
+        <div className="expanded">
           A Vuit Seguros é uma corretora que assessora as empresas, fornecendo
           seguro de vida em grupo e acidentes pessoais, permitindo que
           economizem tempo, esforço e dinheiro.
