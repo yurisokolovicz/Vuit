7ea8ce15dc56824a4f74d8811c730bdce483f0dc
yurisokolovicz <yurisokolovicz@gmail.com>
2023-09-20 12:39:38 -0300

    Beneficios finalizada, apenas conectar backend

--- Frontend/src/pages/Beneficios/Beneficios.js
+++ Frontend/src/pages/Beneficios/Beneficios.js
@@ -16,26 +16,41 @@ import ListaReutilizavel from "../../components/Conteudo/ListaReutilizavel/Lista
 import ParagListReutilizavel from "../../components/Conteudo/ParagListReutilizavel/ParagListReutilizavel";
 
 const Beneficios = () => {
-  const listaDoBackEnd1 = [
-    "IMPLANTAÇÃO DIFERENCIADA AOS CLIENTES",
-    "PRATICIDADE NA GESTAO DOS BENEFÍCIOS E RELATÓRIOS",
-    "POSSIBILIDADE DE NEGOCIAÇÃO FINANCEIRA",
-    "A SODEXO POSSUI VASTA REDE CREDENCIADA EM TODO TERRITÓRIO NACIONAL",
-    "REDUÇÃO DA FOLHA DE PAGAMENTO",
-    "REDUÇÃO DO TEMPO DE RECRUTAMENTO",
-    "REDUÇÃO COM ABSENTEÍSMOS",
+  // ParagListReutilizavel data from the backend
+  const tituloDoBackEndParagList1 = [
+    "A Vuit Benefícios é uma empresa do Grupo Vuit responsável pela implantação de benefícios para milhares de colaboradores de empresas de todo país.",
   ];
 
-  const listaDoBackEnd2 = [
-    "SEGUROS DE VIDA EM GRUPO",
-    "ACIDENTES PESSOAIS",
-    "SEGURO GARANTIA E GARANTIA JUDICIAL",
-    "RESPONSABILIDADE CIVIL E PRESTAÇÃO DE SERVIÇOS",
-    "SEGURO DE SAUDE, DENTAL E TELEMEDICINA",
-    "CRÉDITO CONSIGNADO (VUIT CRED)",
+  const listaDoBackEndParagList1 = [];
+
+  const tituloDoBackEndParagList2 = [
+    "Devido à capilaridade de clientes distribuídos em todos os estados brasileiros, é possível conquistar reduções nas taxas, nos custos e personalizar processos.",
   ];
 
-  const listaDoBackEnd3 = ["Custos reduzidos", "Processos personalizados"];
+  const listaDoBackEndParagList2 = [
+    "Custos reduzidos",
+    "Processos personalizados",
+  ];
+
+  // ListaReutilizavel data from the backend
+  const listaDoBackEndListReut1 = [
+    "Implantação diferenciada aos clientes",
+    "Praticidade na gestão dos benefícios e relatórios",
+    "Possibilidade de negociação financeira",
+    "A sodexo possui vasta rede credenciada em todo território nacional",
+    "Redução da folha de pagamento",
+    "Redução do tempo de recrutamento",
+    "Redução com absenteísmos",
+  ];
+
+  const listaDoBackEndListReut2 = [
+    "Seguros de vida em grupo",
+    "Acidentes pessoais",
+    "Seguro garantia e garantia judicial",
+    "Responsabilidade civil e prestação de serviços",
+    "Seguro de Saúde, dental e telemedicina",
+    "crédito consignado (vuit cred)",
+  ];
 
   return (
     <div id="beneficios">
@@ -54,26 +69,26 @@ const Beneficios = () => {
           src={require("../../img/Homepage/block_images/vuit-beneficios.png")}
         />
         <ListaReutilizavel
-          listaDoBackEnd={[]}
-          titulo="A Vuit Benefícios é uma empresa do Grupo Vuit responsável pela implantação de benefícios para milhares de colaboradores de empresas de todo país."
+          titulo={tituloDoBackEndParagList1}
+          listaDoBackEnd={listaDoBackEndParagList1}
         />
         <BotaoProposta texto="Solicite uma proposta" />
         <Separacao />
         <Baner src={require("../../img/cred/block_images/vuit-escolha.png")} />
-        <ListaReutilizavel listaDoBackEnd={listaDoBackEnd1} />
+        <ListaReutilizavel listaDoBackEnd={listaDoBackEndListReut1} />
         <BotaoProposta texto="Solicite uma proposta" />
         <Separacao />
         <Baner src={require("../../img/cred/block_images/vuit-nacional.png")} />
         <ParagListReutilizavel
-          listaDoBackEnd={listaDoBackEnd3}
-          titulo="Devido à capilaridade de clientes distribuídos em todos os estados brasileiros, é possível conquistar reduções nas taxas, nos custos e personalizar processos."
+          titulo={tituloDoBackEndParagList2}
+          listaDoBackEnd={listaDoBackEndParagList2}
         />
         <BotaoProposta texto="Solicite uma proposta" />
         <Separacao />
         <Baner
           src={require("../../img/beneficios/block_images/vuit-outros-servicos.png")}
         />
-        <ListaReutilizavel listaDoBackEnd={listaDoBackEnd2} />
+        <ListaReutilizavel listaDoBackEnd={listaDoBackEndListReut2} />
         <BotaoProposta texto="Solicite uma proposta" />
         <Separacao />
         <ContatosBeneficios />
